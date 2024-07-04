cc9660c1c3851c298620bf2617dd488f31b987e9
JCampayo <josete10.ab.jc@gmail.com>
2024-07-04 21:47:33 +0200

    Calculadora

--- Hola mundo/app.js
+++ Hola mundo/app.js
@@ -1,2 +1,37 @@
-console.log('Hola perros');
-console.log(2+2);
+
+var btnDisplay = document.getElementsByName("display");
+var value;
+function limpiarResultado() {
+    
+     btnDisplay = document.getElementsByName("display");
+    
+    btnDisplay[0].innerHTML = "0";
+}
+
+function ponerNumero(valor) {
+    
+    
+
+    if (btnDisplay[0].innerText === '0' && value !== '.') {
+        btnDisplay[0].innerText = valor;
+    } else {
+        btnDisplay[0].innerText += valor;
+    }
+
+}
+
+function calculate() {
+    
+    try {
+        btnDisplay[0].innerText = eval(btnDisplay[0].innerText);
+    } catch {
+        alert("Error");
+    }
+
+}
+
+function eliminarNumero() {
+    
+    btnDisplay[0].innerText =  btnDisplay[0].innerText.slice(0, -1);
+
+}
